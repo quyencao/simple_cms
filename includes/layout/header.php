578213939fb2c006a9e587253c9ea77c22c8b99e
QuyenCao <boy20195@gmail.com>
2017-06-17 21:46:28 +0700

    make 2 page admin and manage_content and use include, require to reuse function, layout

--- includes/layout/header.php
+++ includes/layout/header.php
@@ -4,9 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Template</title>
+  <title>Widget Corp</title>
+  <link rel="stylesheet" href="./stylesheets/public.css">
 </head>
 <body>
-
-</body>
-</html>
+ <div id="header">
+   <h1>Widget Corp</h1>
+ </div>
---
+++ public/manage_content.php
@@ -0,0 +1,14 @@
+<?php require_once("../includes/functions.php") ?>
+<?php include("../includes/layout/header.php") ?>
+ <div id="main">
+   <div id="navigation">
+     &nbsp;
+   </div>
+   <div id="page">
+     <h2>Manage Content</h2>
+   </div>
+ </div>
+ <div id="footer">Copyright 20xx, Widget Corp</div>
+</body>
+</html>
+<?php include("../includes/layout/footer.php") ?>
